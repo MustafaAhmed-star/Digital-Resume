c7fa95881544ca59c3fcf25c0e3d267db04a9535
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-03-16 22:00:49 +0200

    add : main conf

--- resume/settings.py
+++ resume/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'resume.context_processor.project_context',
 ]
 
 ROOT_URLCONF = 'resume.urls'
