cae4b102d4b213b75aa3169f8089f946b09d2e6a
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-03-17 14:47:33 +0200

    add : signals to create profile

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
 
@@ -24,6 +26,15 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+        
+    @receiver(post_save,sender=User)
+    def createProfile(sender,instance,created,**kwargs):
+        if created:
+            UserProfile.objects.create(
+                user = instance,
+                
+            
+        )    
         
 class ContactProfile(models.Model):
     class Meta:
